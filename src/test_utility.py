cf993ca335073489fc043e604353137e3fc209b0
Brian <brianpoelstra@gmail.com>
2025-01-28 21:10:20 -0500

    added split delimiter methods to utility.py need to add testing for these methods

--- src/test_utility.py
+++ src/test_utility.py
@@ -6,14 +6,15 @@ from textnode import TextType
 
 class TestUtility(unittest.TestCase):
     def test_eq(self):
-        text_node1=TextNode("hi", TextType.BOLD)
-        text_node2=TextNode("", TextType.LINKS, "www.blah.com")
-        text_node3=TextNode("text", TextType.IMAGES, "www.imagefromhere")
+        #text_node1=TextNode("hi", TextType.BOLD)
+        #text_node2=TextNode("", TextType.LINKS, "www.blah.com")
+        #text_node3=TextNode("text", TextType.IMAGES, "www.imagefromhere")
 
-        print(text_node_to_html_node(text_node1).to_html())
-        print(text_node_to_html_node(text_node2).to_html())
-        print(text_node_to_html_node(text_node3).to_html())
+        #print(text_node_to_html_node(text_node1).to_html())
+        #print(text_node_to_html_node(text_node2).to_html())
+        #print(text_node_to_html_node(text_node3).to_html())
 
+        
 
 
 if __name__=="__main__":
--- src/utility.py
+++ src/utility.py
@@ -26,3 +26,40 @@ def text_node_to_html_node(text_node):
         return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
 
     raise Exception("invalid text type")
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes=[]
+
+    for node in old_nodes:
+        if node.find(delimiter)==-1:
+            raise Exception(f"delimiter not found in {node.to_html()}")
+        if node.text_type!=TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            new_nodes.extend(find_delimiter(node.text, delimiter, text_type))
+
+        return new_nodes
+
+
+def find_delimiter(text, delimiter, text_type):
+    nodes=[]
+    if text.find(delimiter)==-1:
+        return TextNode(text, TextType.TEXT)
+
+    start=text.find(delimiter)
+    end=text.find(delimiter, start+1)
+
+    if start>0:
+        nodes.append(TextNode(text[:start], TextType.TEXT))
+    nodes.append(TextNode(text[start+1:end], text_type))
+
+    if len(text)==end+1:
+        return nodes
+    else:
+        nodes.extend(find_delimiter(text[end+1:], delimiter, text_type))
+
+    return nodes
+
+
+
